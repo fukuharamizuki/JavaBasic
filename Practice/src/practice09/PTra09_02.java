186e24c24531ef43743b14408f98b7554d86b9c6
fukuhara mizuki <mizuki.as2o.0607@gmail.com>
2018-02-28 18:48:52 +0900

    9-2

--- Practice/src/practice09/PTra09_02.java
+++ Practice/src/practice09/PTra09_02.java
@@ -1,4 +1,7 @@
 package practice09;
+
+import practice09.util.Util;
+
 /*
  * PTra09_02.java
  *   作成	LIKEIT	2017
@@ -12,9 +15,11 @@ public class PTra09_02 {
 
 		// ★ 配列arrayを、Util内のメソッドを用いてランダムに並び替えてください
 		// ※ Utilはインポートしても良いし、完全限定名で記述しても良いです
-
+		String[] sarray = Util.shuffleArray(array);
 
 		// ★ ランダムに並び替えた後の配列を前から順に全て表示してください
-
+		for(String i : sarray) {
+			System.out.println(i);
+		}
 	}
 }
